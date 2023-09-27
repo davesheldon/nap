f91135b2465e09874f5206619856c4bf70eb9ce2
Dave Sheldon <dave.sheldon@enhanced.software>
2023-09-27 10:26:56 -0400

    fix: file headers

--- napcapture/capture.go
+++ napcapture/capture.go
@@ -13,7 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 
-assert.go - this file contains types and logic for evaluating assertions
+capture.go - this file contains logic for evaluating captures
 */
 package napcapture
 
--- napquery/query.go
+++ napquery/query.go
@@ -13,7 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 
-runner.go - this file contains logic for running requests, routines and scripts
+query.go - this file contains logic for evaluating queries
 */
 package napquery
 
