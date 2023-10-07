25e6665f6c13739ca41708106fe6473a389ab194
Dave Sheldon <dave.sheldon@enhanced.software>
2023-10-06 23:27:05 -0400

    build: windows zip

--- build.sh
+++ build.sh
@@ -2,6 +2,20 @@
 set -e
 
 GOOS=windows GOARCH=amd64 go build -o ./dist/windows/amd64/nap.exe
+perl -e '
+  use strict;
+  use warnings;
+  use autodie;
+  use IO::Compress::Zip qw(:all);
+  zip [
+    "./dist/windows/amd64/nap.exe"
+  ] => "dist/windows/amd64/nap.zip",
+       FilterName => sub { s[^src/][] },
+       Zip64 => 0,
+  or die "Zip failed: $ZipError\n";
+'
+rm -rf ./dist/windows/amd64/nap.exe
+
 GOOS=darwin GOARCH=amd64 go build -o ./dist/macos/amd64/nap
 tar -czvf ./dist/macos/amd64/nap.tar.gz ./dist/macos/amd64/nap
 rm -rf ./dist/macos/amd64/nap
